2c7645dfa391646667a960a172bd22e059571255
Rubinder Singh <mail2rubinder@gmail.com>
2021-05-30 09:47:34 +1000

    added problem MaxCounters

---
+++ src/Problems/MaxCounters/Solution.cs
@@ -0,0 +1,7 @@
+
+public static partial class Solution {
+  public static int[] MaxCounters_0(int N, int[] A) {
+    int[] a = new int[N];
+    return a;
+  }
+}
---
+++ src/Problems/MaxCounters/Test.cs
@@ -0,0 +1,7 @@
+public static partial class Test {
+  public static bool MaxCountersTest() {
+    var didTestsPass = true;
+
+    return didTestsPass;
+  }
+}
