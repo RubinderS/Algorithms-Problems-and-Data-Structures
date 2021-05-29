2d41298137eaf6aaf63d2ef32297f020b98769bc
Rubinder Singh <mail2rubinder@gmail.com>
2021-05-30 00:01:05 +1000

    Completed Solution for FrogRiverOne problem

--- src/Problems/FrogRiverOne/Solution.cs
+++ src/Problems/FrogRiverOne/Solution.cs
@@ -1,15 +1,14 @@
 using System.Collections.Generic;
-using System.Linq;
 
 public static partial class Solution {
   public static int FrogRiverOne0(int X, int[] A) {
-    int[] arr = new int[X];
+    HashSet<int> set = new HashSet<int>();
 
     for (int i = 0; i < A.Length; i++) {
       var leafPosition = A[i];
-      arr[leafPosition - 1] = 1;
+      set.Add(leafPosition - 1);
 
-      if (!arr.Contains(0)) {
+      if (set.Count == X) {
         return i;
       }
     }
