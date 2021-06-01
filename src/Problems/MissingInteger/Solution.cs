ab8f416b0cb1eac0f0540a26af829cb75dfac8d2
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-01 13:28:07 +1000

    Completed solution for MissingIntergers

---
+++ src/Problems/MissingInteger/Solution.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static partial class Solution {
+  public static int MissingInteger(int[] A) {
+    var smallestMissingInteger = 1;
+    Array.Sort(A);
+
+    foreach (var integer in A) {
+      if (integer == smallestMissingInteger) {
+        smallestMissingInteger++;
+      }
+    }
+
+    return smallestMissingInteger;
+  }
+}
---
+++ src/Problems/MissingInteger/Test.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+public static partial class Test {
+  public static bool MissingIntegerTest() {
+    var didTestsPass = true;
+
+    if (Solution.MissingInteger(new int[] { 1, 3, 6, 4, 1, 2 }) != 5) {
+      didTestsPass = false;
+    }
+
+    if (Solution.MissingInteger(new int[] { 1, 2, 3 }) != 4) {
+      didTestsPass = false;
+    }
+
+    if (Solution.MissingInteger(new int[] { -1, -3 }) != 1) {
+      didTestsPass = false;
+    }
+
+    return didTestsPass;
+  }
+}
