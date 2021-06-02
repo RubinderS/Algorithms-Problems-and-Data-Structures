8e7d24a51a862be7ca2964373fa6202c5a4cb676
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-02 13:00:26 +1000

    Added test cases for PermCheck

--- src/Problems/PermCheck/Test.cs
+++ src/Problems/PermCheck/Test.cs
@@ -1,9 +1,17 @@
+using System.Linq;
+
 public static partial class Test {
   public static bool PermCheckTest() {
     var didTestsPass = true;
 
+    if (Solution.PermCheck(new int[] { 4, 1, 3, 2 }) != 1) {
+      didTestsPass = false;
+    }
+
+    if (Solution.PermCheck(new int[] { 4, 1, 3 }) != 1) {
+      didTestsPass = false;
+    }
+
     return didTestsPass;
-
   }
-
 }
