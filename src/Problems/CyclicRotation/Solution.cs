6026a0b069e93d7ab5fa8e6118bae6ae5ea759b6
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-03 23:58:15 +1000

    added problem CyclicRotation

---
+++ src/Problems/CyclicRotation/Solution.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+public class CyclicRotation {
+  public int[] Solution(int[] A, int K) {
+    return new int[0];
+  }
+
+  [Theory]
+  [InlineData(new[] { 4, 1, 3, 2 }, 3, new[] { 9, 7, 6, 3, 8 })]
+  [InlineData(new[] { 0, 0, 0 }, 1, new[] { 0, 0, 0 })]
+  [InlineData(new[] { 1, 2, 3, 4 }, 4, new[] { 1, 2, 3, 4 })]
+  public void Test(int[] value1, int value2, int[] expected) {
+    Assert.Equal(expected, Solution(value1, value2));
+  }
+}
