bd18550505da208ad0c49db115afce8ae00c9e85
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-04 09:51:25 +1000

    corrected test case

--- src/Problems/CyclicRotation/Solution.cs
+++ src/Problems/CyclicRotation/Solution.cs
@@ -6,7 +6,7 @@ public class CyclicRotation {
   }
 
   [Theory]
-  [InlineData(new[] { 4, 1, 3, 2 }, 3, new[] { 9, 7, 6, 3, 8 })]
+  [InlineData(new[] { 3, 8, 9, 7, 6 }, 3, new[] { 9, 7, 6, 3, 8 })]
   [InlineData(new[] { 0, 0, 0 }, 1, new[] { 0, 0, 0 })]
   [InlineData(new[] { 1, 2, 3, 4 }, 4, new[] { 1, 2, 3, 4 })]
   public void Test(int[] value1, int value2, int[] expected) {
