bc7db582022d803054e390f73a3b6b2cdce231f0
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-05 11:49:38 +1000

    Added problem FrogJmp

---
+++ src/Problems/FrogJmp/Solution.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+public class FrogJmp {
+  public int Solution(int X, int Y, int D) {
+    var numberOfJumps = 0;
+    return numberOfJumps;
+  }
+
+  [Theory]
+  [InlineData(10, 85, 30, 3)]
+  [InlineData(5, 25, 5, 4)]
+  public void Test(int value1, int value2, int value3, int expected) {
+    Assert.Equal(expected, Solution(value1, value2, value3));
+  }
+}
