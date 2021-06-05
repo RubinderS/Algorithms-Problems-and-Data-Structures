a4b9db81971bcd27cb17f05d5d09feafe5e3c077
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-06 01:05:42 +1000

    Added problem CountDiv

---
+++ src/Problems/Medium/CountDiv/Solution.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+public class CountDiv {
+  public int Solution(int A, int B, int K) {
+    return 0;
+  }
+
+  [Theory]
+  [InlineData(6, 11, 2, 3)]
+  public void Test(int value1, int value2, int value3, int expected) {
+    Assert.Equal(expected, Solution(value1, value2, value3));
+  }
+}
