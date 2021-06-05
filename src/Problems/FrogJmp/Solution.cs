18accfa0ae370a5a53a8970567bee41c8e6d39e4
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-05 11:50:40 +1000

    Added test cases to the problem FrogJmp

--- src/Problems/FrogJmp/Solution.cs
+++ src/Problems/FrogJmp/Solution.cs
@@ -9,6 +9,8 @@ public class FrogJmp {
   [Theory]
   [InlineData(10, 85, 30, 3)]
   [InlineData(5, 25, 5, 4)]
+  [InlineData(88, 89, 7, 1)]
+  [InlineData(65, 65, 7, 0)]
   public void Test(int value1, int value2, int value3, int expected) {
     Assert.Equal(expected, Solution(value1, value2, value3));
   }
