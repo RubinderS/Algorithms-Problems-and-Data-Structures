00ab6965a48581f0f1869ac49defd4dcce2442f1
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-11 00:06:48 +1000

    wip

--- src/Problems/Medium/MushroomPicker/Solution.cs
+++ src/Problems/Medium/MushroomPicker/Solution.cs
@@ -1,7 +1,27 @@
+using System;
 using Xunit;
 
 public class MushroomPicker {
   public int Solution(int[] A, int K, int M) {
+    int[] PrefixSums(int[] Arr) {
+      var prefixSums = new int[Arr.Length];
+      prefixSums[0] = Arr[0];
+
+      for (int i = 1; i < Arr.Length; i++) {
+        prefixSums[i] = prefixSums[i - 1] + Arr[i];
+      }
+
+      return prefixSums;
+    }
+
+    int CalcSum(int[] prefixArr, int x, int y) {
+      if (x <= 1) {
+        return prefixArr[y];
+      }
+
+      return prefixArr[y] - prefixArr[x - 1];
+    };
+
     return 0;
   }
 
