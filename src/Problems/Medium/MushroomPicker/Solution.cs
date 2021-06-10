b35b5eedff7f33bf9236418a15128066834b3149
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-11 09:30:42 +1000

    completed solution for Mushroom Picker

--- src/Problems/Medium/MushroomPicker/Solution.cs
+++ src/Problems/Medium/MushroomPicker/Solution.cs
@@ -2,7 +2,7 @@ using System;
 using Xunit;
 
 public class MushroomPicker {
-  public int Solution(int[] A, int K, int M) {
+  public int Solution(int[] Mushrooms, int StartingPosition, int Moves) {
     int[] PrefixSums(int[] Arr) {
       var prefixSums = new int[Arr.Length];
       prefixSums[0] = Arr[0];
@@ -20,9 +20,25 @@ public class MushroomPicker {
       }
 
       return prefixArr[y] - prefixArr[x - 1];
-    };
+    }
 
-    return 0;
+    var maxMushrooms = 0;
+    var prefixSums = PrefixSums(Mushrooms);
+
+    for (int i = 0; i < Math.Min(StartingPosition, Moves); i++) {
+      int leftPos = StartingPosition - i;
+      int rightPos = Math.Min(Math.Max(StartingPosition + Moves - 2 * i, StartingPosition), Mushrooms.Length - 1);
+      maxMushrooms = Math.Max(maxMushrooms, CalcSum(prefixSums, leftPos, rightPos));
+    }
+
+    for (int i = 0; i < Math.Min(Mushrooms.Length - StartingPosition, Moves); i++) {
+      int rightPos = StartingPosition + i;
+      int leftPos = Math.Max(Math.Min(StartingPosition - Moves + 2 * i, StartingPosition), 0);
+      maxMushrooms = Math.Max(maxMushrooms, CalcSum(prefixSums, leftPos, rightPos));
+    }
+
+
+    return maxMushrooms;
   }
 
   [Theory]
