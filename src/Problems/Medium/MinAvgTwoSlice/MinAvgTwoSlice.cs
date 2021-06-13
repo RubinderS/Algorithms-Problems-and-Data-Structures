95b769158f11d7185f7f1762168f9c00e518f131
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-13 20:20:16 +1000

    completed solution for MinAvgTwoSlice

--- src/Problems/Medium/MinAvgTwoSlice/MinAvgTwoSlice.cs
+++ src/Problems/Medium/MinAvgTwoSlice/MinAvgTwoSlice.cs
@@ -1,8 +1,37 @@
+using System;
 using Xunit;
 
 public class MinAvgTwoSlice {
   public int Solution(int[] A) {
-    return 0;
+    double currAvg1 = Double.MaxValue;
+    double currAvg2 = Double.MaxValue;
+    double minAvg = Double.MaxValue;
+    int minAvgIndex = 0;
+
+    for (int i = 0; i < A.Length - 2; i++) {
+      currAvg1 = (A[i] + A[i + 1]) / 2.0;
+
+      if (currAvg1 < minAvg) {
+        minAvg = currAvg1;
+        minAvgIndex = i;
+      }
+
+      currAvg2 = (currAvg1 + A[i + 2]) / 2.0;
+
+      if (currAvg2 < minAvg) {
+        minAvg = currAvg2;
+        minAvgIndex = i;
+      }
+    }
+
+    currAvg1 = (A[A.Length - 1] + A[A.Length - 2]) / 2.0;
+
+    if (currAvg1 < minAvg) {
+      minAvg = currAvg1;
+      minAvgIndex = A.Length - 2;
+    }
+
+    return minAvgIndex;
   }
 
   [Theory]
