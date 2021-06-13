5e6b7bb00e37d6719afa8ee49a18ea164dc787c4
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-14 00:29:00 +1000

    Completed solution for PassingCars

--- src/Problems/Easy/PassingCars/PassingCars.cs
+++ src/Problems/Easy/PassingCars/PassingCars.cs
@@ -2,14 +2,30 @@ using Xunit;
 
 public class PassingCars {
   public int Solution(int[] A) {
-    return 0;
+    var nCarsEast = 0;
+    var pairs = 0;
+
+    foreach (var direction in A) {
+      if (direction == 0) {
+        nCarsEast++;
+      } else {
+        pairs += nCarsEast;
+
+        if (pairs > 1000000000) {
+          return -1;
+        }
+      }
+    }
+
+
+    return pairs;
   }
 
   [Theory]
   [InlineData(new[] { 0, 1, 0, 1, 1 }, 5)]
   [InlineData(new[] { 0, 0, 0 }, 0)]
   [InlineData(new[] { 1, 1, 1 }, 0)]
-  public void Test(int value1, int expected) {
-    Assert.Equal(expected, value1);
+  public void Test(int[] value1, int expected) {
+    Assert.Equal(expected, Solution(value1));
   }
 }
