d50f41524d01c319d3dac04e8f0fd2bf6881b5aa
Rubinder Singh <mail2rubinder@gmail.com>
2021-06-13 21:03:47 +1000

    Added test cases

--- src/Problems/Medium/PassingCars/PassingCars.cs
+++ src/Problems/Medium/PassingCars/PassingCars.cs
@@ -7,6 +7,8 @@ public class PassingCars {
 
   [Theory]
   [InlineData(new[] { 0, 1, 0, 1, 1 }, 5)]
+  [InlineData(new[] { 0, 0, 0 }, 0)]
+  [InlineData(new[] { 1, 1, 1 }, 0)]
   public void Test(int value1, int expected) {
     Assert.Equal(expected, value1);
   }
